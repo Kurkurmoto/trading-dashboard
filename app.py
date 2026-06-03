492c433f016f0d2ba3cbe5be73141de1eaa0838f
Kurkurmoto <realraushankumar495@gmail.com>
2026-06-03 21:16:36 +0530

    Render deployment fix

--- app.py
+++ app.py
@@ -32,5 +32,8 @@ def history():
 
     return jsonify({"candles": candles})
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
